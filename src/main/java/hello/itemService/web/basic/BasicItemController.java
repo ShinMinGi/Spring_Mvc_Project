ebda45eee0d7d3aa3663517335e82587657a5942
ShinMinGi <140944460+ShinMinGi@users.noreply.github.com>
2025-01-20 15:50:04 +0900

    상품 상세 개발

--- src/main/java/hello/itemService/web/basic/BasicItemController.java
+++ src/main/java/hello/itemService/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,13 +28,23 @@ public class BasicItemController {
         return "basic/items";
     }
 
+
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init() {
-        itemRepository.save(new Item("itemA", 10000,  10));
-        itemRepository.save(new Item("itmeB", 20000, 20));
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
     }
-
 }
